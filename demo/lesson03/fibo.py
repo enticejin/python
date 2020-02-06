89aacec7d14806c39c11dc483d5a07ea9ce6beee
少爷辉 <www.403367632@qq.com>
2020-02-06 18:37:30 +0800

    模块的运用

--- demo/lesson03/fibo.py
+++ demo/lesson03/fibo.py
@@ -1,14 +1,31 @@
-def fib(n):
+# def fib(n):
+#     a, b = 0, 1
+#     while a < n:
+#         print(a, end=' ')
+#         a, b = b, a + b
+#     print()
+#
+# def fib2(n):
+#     result = []
+#     a, b = 0, 1
+#     while a < n:
+#         result.append(a)
+#         a , b = b, a + b
+#     return result
+
+# Fibonacci numbers module
+
+def fib(n):    # write Fibonacci series up to n
     a, b = 0, 1
     while a < n:
         print(a, end=' ')
-        a, b = b, a + b
+        a, b = b, a+b
     print()
 
-def fib2(n):
+def fib2(n):   # return Fibonacci series up to n
     result = []
     a, b = 0, 1
     while a < n:
         result.append(a)
-        a , b = b, a + b
+        a, b = b, a+b
     return result
