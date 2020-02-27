ab60bd5e9bfb54f620f9177c98484310cb99c145
少爷辉 <www.403367632@qq.com>
2020-02-27 22:23:44 +0800

    format学习

---
+++ python-xiaojiayu/format-test.py
@@ -0,0 +1,11 @@
+'''
+format学习
+'''
+#print("{0} love {1}.{2}".format("I", "FishC", "com"))
+print("{a} love {b}.{c}".format(a = "I", b = "FishC", c = "com"))
+#转义字符
+print("\\")
+#ASCII码 %c
+print("%c" % 97) #a
+
+print('%c %c %c' %(97, 98, 99)) #a b c
