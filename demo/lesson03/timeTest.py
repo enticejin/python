3048510d5b72a81de3ef08ed101dfc872ed00531
Administrator <www.403367632@qq.com>
2020-03-10 16:19:14 +0800

    添加方法

--- demo/lesson03/timeTest.py
+++ demo/lesson03/timeTest.py
@@ -37,4 +37,17 @@ Mo Tu We Th Fr Sa Su
 30 31
 '''
 
+def fib(n):
+    a , b = 0, 1
+    while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+    print()
 
+def fib2(n):
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b , a+b
+    return result
