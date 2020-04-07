fa3b6db9093669eb104bf95e3445b07afa915bc8
Administrator <www.403367632@qq.com>
2020-04-07 17:06:13 +0800

    python读文件

--- demo/lesson07/读写文件.py
+++ demo/lesson07/读写文件.py
@@ -1,8 +1,21 @@
+import csv
+import matplotlib.pyplot as plt
+import re
 f = open(R"D:\work\pointinfo_solve.csv")
 #f = open("D:/work/pointinfo_solve.csv")
 #print(f.read())
 
-with open("D:/work/pointinfo_solve.csv") as x:
-    x_read = x.read()
-    print(x_read)
-x.close()
+filename = "D:/work/pointinfo_solve.csv"
+list1 = []
+x_list = []
+y_list = []
+i = 0
+with open(filename, 'r') as file:
+    reader = csv.DictReader(file)
+    column = [row['-11.54177605817667|-3.553084689684635|1.0'] for row in reader]
+    for element in column:
+        x_list.append(re.split("\\|", column[i])[0])
+        y_list.append(re.split("\\|", column[i])[1])
+        i =i+1;
+print(x_list)
+print(y_list)
