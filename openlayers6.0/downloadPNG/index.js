c18b362f8cfbda1000f3232562596e729e92813b
Administrator <www.403367632@qq.com>
2020-05-19 14:38:52 +0800

    参照openlayers6.0官网npm范例

--- openlayers6.0/downloadPNG/index.js
+++ openlayers6.0/downloadPNG/index.js
@@ -33,29 +33,26 @@ document.getElementById('export-png').addEventListener('click', function () {
         mapCanvas.width = size[0];
         mapCanvas.height = size[1];
         var mapContext = mapCanvas.getContext('2d');
-    Array.prototype.forEach.call(document.querySelectorAll('.ol-layer canvas'), function(canvas) {
-      if (canvas.width > 0) {
-        var opacity = canvas.parentNode.style.opacity;
-        mapContext.globalAlpha = opacity === '' ? 1 : Number(opacity);
-        var transform = canvas.style.transform;
-        // Get the transform parameters from the style's transform matrix
-        var matrix = transform.match(/^matrix\(([^\(]*)\)$/)[1].split(',').map(Number);
-        // Apply the transform to the export map context
-        CanvasRenderingContext2D.prototype.setTransform.apply(mapContext, matrix);
-        mapContext.drawImage(canvas, 0, 0);
-      }
+        Array.prototype.forEach.call(document.querySelectorAll('.ol-layer canvas'), function (canvas) {
+            if (canvas.width > 0) {
+                var opacity = canvas.parentNode.style.opacity;
+                mapContext.globalAlpha = opacity === '' ? 1 : Number(opacity);
+                var transform = canvas.style.transform;
+                // Get the transform parameters from the style's transform matrix
+                var matrix = transform.match(/^matrix\(([^\(]*)\)$/)[1].split(',').map(Number);
+                // Apply the transform to the export map context
+                CanvasRenderingContext2D.prototype.setTransform.apply(mapContext, matrix);
+                mapContext.drawImage(canvas, 0, 0);
+            }
+        });
+        if (navigator.msSaveBlob) {
+            // link download attribuute does not work on MS browsers
+            navigator.msSaveBlob(mapCanvas.msToBlob(), 'map.png');
+        } else {
+            var link = document.getElementById('image-download');
+            link.href = mapCanvas.toDataURL();
+            link.click();
+        }
     });
-    if (navigator.msSaveBlob) {
-      // link download attribuute does not work on MS browsers
-      navigator.msSaveBlob(mapCanvas.msToBlob(), 'map.png');
-    } else {
-      var link = document.getElementById('image-download');
-      link.href = mapCanvas.toDataURL();
-      link.click();
-    }
-  });
-  map.renderSync();
-
-    })
-
+    map.renderSync();
 })
