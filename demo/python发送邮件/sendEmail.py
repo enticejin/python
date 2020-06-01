82b4fdd3d854a3a8d6b93324cca11bb518463b23
Administrator <www.403367632@qq.com>
2020-06-01 17:57:33 +0800

    python发送邮件验证码

--- demo/python发送邮件/sendEmail.py
+++ demo/python发送邮件/sendEmail.py
@@ -1,17 +1,45 @@
 
-import smtplib
+import smtplib,random
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
 my_sender = 'www.403367632@qq.com'  # 发件人邮箱账号
-my_pass = '1995owzosdknhrqmbehg0404'  # 发件人邮箱密码
+my_pass = '1994owzosdknhrqmbehg0525'  # 发件人邮箱密码
 my_user = 'gejinjin0404@foxmail.com'  # 收件人邮箱账号，我这边发送给自己
 
+
+def generate_verification_code():
+
+    ''' 随机生成6位的验证码 '''
+
+    code_list = []
+
+    for i in range(10): # 0-9数字
+
+        code_list.append(str(i))
+
+    for i in range(65, 91): # A-Z
+
+        code_list.append(chr(i))
+
+    for i in range(97, 123): # a-z
+
+        code_list.append(chr(i))
+    myslice = random.sample(code_list, 6)  # 从list中随机获取6个元素，作为一个片断返回
+
+    verification_code = ''.join(myslice) # list to string
+
+    # print code_list
+
+    # print type(myslice)
+
+    return verification_code
 
 def mail():
     ret = True
     try:
-        msg = MIMEText('python 测试发送邮件', 'plain', 'utf-8')
+        code = generate_verification_code()
+        msg = MIMEText('验证码是： '+code, 'plain', 'utf-8')
         msg['From'] = formataddr(["FromRunoob", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
         msg['To'] = formataddr(["FK", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
         msg['Subject'] = "菜鸟教程发送邮件测试"  # 邮件的主题，也可以说是标题
@@ -30,3 +58,4 @@ if ret:
     print("邮件发送成功")
 else:
     print("邮件发送失败")
+
