3985a09f784830654b6e1876f44a68e4f1b95440
Administrator <www.403367632@qq.com>
2020-07-31 17:48:15 +0800

    画心

---
+++ demo/画方格/heart.py
@@ -0,0 +1,26 @@
+import turtle
+
+def draw():
+    def curvemove():
+        for i in range(200):
+            turtle.right(1)
+            turtle.forward(1)
+
+    turtle.speed(1000)
+    #turtle.tracer(2, 0)
+    turtle.hideturtle()
+
+    turtle.color('red','pink')
+    turtle.begin_fill()
+    turtle.left(140)
+    turtle.forward(111.65)
+    curvemove()
+    turtle.left(120)
+    curvemove()
+    turtle.forward(111.65)
+    turtle.end_fill()
+
+
+if __name__ == "__main__":
+    draw()
+    turtle.mainloop()
